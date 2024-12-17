2a80460236d1653f3b5c791fa46ade741e0a385e
haihuijiaba <156080934+haihuijiaba@users.noreply.github.com>
2024-12-17 16:44:19 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,8 +1,8 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
-let proxyIP = '';
+let userID = 'c8d39b87-8a0b-479e-9255-2030fb47410d';
+let proxyIP = 'cdn.xn--b6gac.eu.org';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
